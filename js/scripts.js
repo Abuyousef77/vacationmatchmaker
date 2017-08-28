57a42c1cc7e469a78b5bf9d80087c5c1612acb30
Benjamin Bressler and Sean Slingerland <benjamin+sean@>
2017-08-28 15:30:43 -0700

    work on functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
 //business interface
+function Country (name, season, cost, environment, history, crowd){
+  this.name = name;
+  this.season = season;
+  this.cost = cost;
+  this.environment = environment;
+  this.crowd = crowd;
+  this.history = history;
+}
 
+var countries = [
+  new Country ("Iceland", "Summer", "Splurge", "Nature", "Modern", "Hate-them"),
+  new Country ("India", "Winter", "Budget", "mix", "Historic", "Like-them"),
+  new Country ("Mexico", "Fall", "Average", "mix", "Historic", "Indifferent"),
+  new Country ("Singapore", "Spring", "Splurge", "City", "Modern", "Like-them"),
+  new Country ("Egypt", "Winter", "Budget", "City", "Historic", "Indifferent"),
+];
+
+Country.prototype.match = function () {
+
+};
 
 //user interface
 $(document).ready(function(){
@@ -11,6 +30,5 @@ $(document).ready(function(){
     var history = $("#history").val();
     var crowds = $("#crowds").val();
 
-    $('#mexico').show();
   });
 });
