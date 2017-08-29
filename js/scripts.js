c2c3f2fb784cad5f41df88ddde62aec56835609b
@mdawg85 <margaretshelaghmcgovern@gmail.com>
2017-08-28 22:47:36 -0700

    change values to lowercase, write vacationmatch function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 //business interface
-function Country (name, season, cost, environment, history, crowd){
-  this.name = name;
+var iceland = [];
+var india = [];
+var mexico = [];
+var singapore = [];
+var egypt = [];
+
+//objects
+function Country (season, cost, environment, history, crowd){
   this.season = season;
   this.cost = cost;
   this.environment = environment;
@@ -9,26 +15,81 @@ function Country (name, season, cost, environment, history, crowd){
 }
 
 var countries = [
-  new Country ("Iceland", "Summer", "Splurge", "Nature", "Modern", "Hate-them"),
-  new Country ("India", "Winter", "Budget", "mix", "Historic", "Like-them"),
-  new Country ("Mexico", "Fall", "Average", "mix", "Historic", "Indifferent"),
-  new Country ("Singapore", "Spring", "Splurge", "City", "Modern", "Like-them"),
-  new Country ("Egypt", "Winter", "Budget", "City", "Historic", "Indifferent"),
+  new Country ("summer", "splurge", "nature", "modern", "hate-them"), //iceland
+  new Country ("winter", "budget", "mix", "historic", "like-them"), //india
+  new Country ("fall", "average", "mix", "historic", "indifferent"), //mexico
+  new Country ("spring", "splurge", "city", "modern", "like-them"), //singapore
+  new Country ("winter", "budget", "city", "historic", "indifferent"), //egypt
 ];
 
-Country.prototype.match = function () {
+Country.prototype.vacationmatch = function () {
+  if (this.season === "summer") {
+    iceland.push(1);
+  } else if (this.season === "winter") {
+    india.push(1);
+    egypt.push(1);
+  } else if (this.season === "fall") {
+    mexico.push(1);
+  } else if (this.season === "spring") {
+    singapore.push(1);
+  }
 
+  if (this.cost === "splurge") {
+    iceland.push(1);
+    singapore.push(1);
+  } else if (this.season === "budget") {
+    india.push(1);
+    egypt.push(1);
+  } else if (this.season === "average") {
+    mexico.push(1);
+  }
+
+  if (this.environment === "nature") {
+    iceland.push(1);
+  } else if (this.environment === "mix") {
+    india.push(1);
+    mexico.push(1);
+  } else if (this.environment === "city") {
+    egypt.push(1);
+    singapore.push(1);
+  }
+
+  if (this.history === "modern") {
+    iceland.push(1);
+    singapore.push(1);
+  } else if (this.history === "historic") {
+    india.push(1);
+    mexico.push(1);
+    egypt.push(1);
+  }
+
+  if (this.crowd === "hate-them"){
+    iceland.push(1);
+  } else if (this.crowd === "indifferent") {
+    mexico.push(1);
+    egypt.push(1);
+  } else if (this.crowd === "like-them") {
+    india.push(1);
+  }
+  alert("Iceland is" + iceland);
+  alert("India is" + india);
+  alert("Egypt is" + egypt);
+  alert("Mexico is" + mexico);
+  alert("Singapore is" + singapore);
 };
 
 //user interface
 $(document).ready(function(){
   $("#questions").submit(function(event) {
     event.preventDefault();
-    var seasons = $("#seasons").val();
-    var costs = $("#costs").val();
-    var environment = $("#environment").val();
-    var history = $("#history").val();
-    var crowds = $("#crowds").val();
+    var userSeason = $("#seasons").val();
+    var userCost = $("#costs").val();
+    var userEnv = $("#environment").val();
+    var userHist = $("#history").val();
+    var userCrowds = $("#crowds").val();
 
+    var myCountry = new Country (userSeason, userCost, userEnv, userHist, userCrowds);
+
+    myCountry.vacationmatch();
   });
 });
